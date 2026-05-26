4b46197c62e2a22cc1da2e002754a9f57371665e
Jesus Lozano <jesus.lozano@adm.interactiveworkers.com>
2026-05-26 16:50:52 -0400

    UI base

---
+++ CarnetGeneratorFACO/Models/Carnet.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CarnetGeneratorFACO.Models;
+
+public class Carnet
+{
+    
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Nh { get; set; }
+    public DateTime ExpDate { get; set; }
+    public string LocationName { get; set; }
+    public int LocationNumber { get; set; }
+    
+    public Carnet(int id, string name, int nh, DateTime expDate, string locationName, int locationNumber)
+    {
+        Id = id;
+        Name = name;
+        Nh = nh;
+        ExpDate = expDate;
+        LocationName = locationName;
+        LocationNumber = locationNumber;
+    }
+}
