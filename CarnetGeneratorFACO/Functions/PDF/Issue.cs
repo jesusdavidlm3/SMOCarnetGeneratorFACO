12f20043212ea15cdb9013e0eefdda2777514904
Jesus Lozano <jesus.lozano@adm.interactiveworkers.com>
2026-06-01 12:08:29 -0400

    Layout de carnet, formato de fecha y tamaño de letra

--- CarnetGeneratorFACO/Functions/PDF/Issue.cs
+++ CarnetGeneratorFACO/Functions/PDF/Issue.cs
@@ -26,31 +26,72 @@ public class IssueCards : IDocument
 
     void ComposeBody(IContainer container)
     {
-        container.Row(p =>
+        container.Column(page =>
         {
-            
-            p.RelativeItem().Column(s =>
+            int CarRowsCount;
+            if (Carnets.Count % 2 == 0)
             {
-                s.Item().Row(row =>
+                CarRowsCount =  Carnets.Count / 2;
+            }
+            else
+            {
+                CarRowsCount = (Carnets.Count + 1) / 2;
+            }
+
+            for (int i = 0; i < CarRowsCount; i++)
+            {
+                page.Item().Row(carRow =>
                 {
-                    row.ConstantItem(7.7f, Unit.Centimetre).Column(car =>
+                    carRow.ConstantItem(8.5f, Unit.Centimetre).Border(1, Colors.Black).Column(carnet =>
                     {
-                        car.Item().Row(carRow =>
+                        carnet.Item().Row(carnetR =>
                         {
-                            carRow.ConstantItem(3f, Unit.Centimetre).Image("Assets/luzlogo.png");
-                            carRow.ConstantItem(3f, Unit.Centimetre).Image("Assets/smologo.png");
-                        });
-                        car.Item().Row(carRow =>
-                        {
-                            carRow.RelativeItem().Text(Carnets[0].Name);         
+                            carnetR.ConstantItem(7.7f, Unit.Centimetre).Column(info =>
+                            {
+                                info.Item().Row(internalRow =>
+                                {
+                                    internalRow.ConstantItem(3f, Unit.Centimetre).Image("Assets/luzlogo.png");
+                                    internalRow.ConstantItem(3f, Unit.Centimetre).Image("Assets/smologo.png");
+                                });
+                                info.Item().Row(internalRow =>
+                                {
+                                    internalRow.RelativeItem().Text($"{Carnets[0].Name}").FontSize(10);
+                                });
+                                info.Item().Row(internalRow =>
+                                {
+                                    internalRow.RelativeItem().Text($"C.I. {Carnets[0].Id}").FontSize(10);
+                                });
+                                info.Item().Row(internalRow =>
+                                {
+                                    internalRow.RelativeItem().Text($"N.H. # {Carnets[0].Nh}   Vencimiento: {Carnets[0].ExpDate.Day}/{Carnets[0].ExpDate.Month}/{Carnets[0].ExpDate.Year}").FontSize(10);
+                                });
+                                info.Item().Row(internalRow =>
+                                {
+                                    internalRow.RelativeItem().Text($"Ubicacion {Carnets[0].LocationNumber}").FontSize(10);
+                                });
+                                info.Item().Row(internalRow =>
+                                {
+                                    internalRow.RelativeItem().Text($"{Carnets[0].LocationName}").FontSize(10);
+                                });
+                            });
+                            carnetR.ConstantItem(0.8f, Unit.Centimetre).Column(c =>
+                            {
+                                c.Item().Image("Assets/cintillo.png");
+                            });
                         });
                     });
                 });
-            });
-            p.ConstantItem(0.8f, Unit.Centimetre).Column(c =>
-            {
-                c.Item().Image("Assets/cintillo.png");
-            });
+            }
+            
+
         });
+        // for (int i = 0; i < Carnets.Count; i++)
+        // {
+        // }
     }
+
+    // private void BuildCarnet(Carnet carnet)
+    // {
+    //     
+    // }
 }
